2162cbf74c33dbf0a57f5a8205b5e3d4ac2324d3
mxi88 <achtermann.maxi@icloud.com>
2026-09-20 19:12:04 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_VERSION = 'mb-tracker-v2';
+const ASSETS = [
+  './mercedes-tracker.html',
+  './manifest.json',
+  './icon-192.svg',
+  './icon-512.svg',
+  './vehicle.webp',
+  './vehicle.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_VERSION).then((cache) =>
+      Promise.all(ASSETS.map((url) => cache.add(url).catch(() => null)))
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_VERSION).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      const network = fetch(e.request)
+        .then((resp) => {
+          if (resp && resp.ok) {
+            caches.open(CACHE_VERSION).then((cache) => cache.put(e.request, resp.clone()));
+          }
+          return resp;
+        })
+        .catch(() => cached);
+      return cached || network;
+    })
+  );
+});
